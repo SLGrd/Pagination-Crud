de7e6caf81276d8ee21cbe3e5d25a2ff46ab7dac
SLGrd <39447468+SLGrd@users.noreply.github.com>
2021-12-27 03:36:25 +0000

    Update Glb.cs

--- Common/Glb.cs
+++ Common/Glb.cs
@@ -5,9 +5,9 @@
         public static string CnnString;
 
         public static  string BaseAddress       = "";
-        //public static string BaseAddressSemSSL = "http://localhost:10639";
-        //public static string BaseAddressComSSL = "http://localhost:44314";
+        //public static string BaseAddressSemSSL = "";
+        //public static string BaseAddressComSSL = "";
 
-        public static readonly string apiKey = @"apiKey=5c2cfa5a9fe6395d6ea0";       
+        public static readonly string apiKey = @"";       
     }
 }
